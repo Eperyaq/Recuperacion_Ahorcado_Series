69ba624c781ccfce053c0741a80286c6cf161e4a
EliaCasa <eperyaq@g.educaand.es>
2024-04-18 09:58:27 +0200

    ahorcado terminado guay

--- src/main/kotlin/Ahorcado.kt
+++ src/main/kotlin/Ahorcado.kt
@@ -1,37 +1,55 @@
 /**
  * Clase que representa el juego del ahorcado
  */
-class Ahorcado(val numIntentos:Int = 5) {
+class Ahorcado(var numIntentos:Int = 5) {
     val consola = GestorConsola()
 
     private val palabraRandom = Palabra().obtenerPalabraAleatoria() //Palabra random escogida
 
     fun jugar() {
             println(palabraRandom)
-            println(ocultarPalabra())
+            ocultarPalabra()
 
     }
 
     private fun ocultarPalabra(){
-        val palabraOcultada = mutableListOf<String>()
-        val palabraSinOcultar= mutableListOf<String>()
 
-       for (letra in palabraRandom){
-           palabraOcultada.add("_")
-           palabraSinOcultar.add("$letra")
-       }
-        consola.mostrarInfo(palabraOcultada.joinToString(" "))
+        var palabraTerminada = false
 
-        val letraMetida = consola.pedirCadena("Intenta con una letra: ")
-
-
-
-        for (i in 1..palabraSinOcultar.size ){ //Aqui estan los indices peta
-            if (letraMetida == palabraSinOcultar[i])
-                palabraOcultada[i] = letraMetida
+        val palabraOcultada = mutableListOf<String>() //Lista con la palabra oculta para mostrarla con los _
+        val palabraSinOcultar = mutableListOf<String>() //Lista con la palabra sin ocultar para comprobar
+        for (letra in palabraRandom) { //Bucle para esconder la palabra y a su vez guardarla en la lista escondida y sin esconder
+            palabraOcultada.add("_")
+            palabraSinOcultar.add("$letra")
         }
+        consola.mostrarInfo(palabraOcultada.joinToString(" "))//Mostrar la palabra ocultada separadas con un espacio
 
+        while (numIntentos > 0 && !palabraTerminada) {
 
+            val letraMetida = consola.pedirCadena("Intenta con una letra: ")
+
+            if (letraMetida in palabraSinOcultar) { //Comprueba si la letra metida está en la lista de la palabra sin ocultar y si es asi la reemplaza en la lista ocultada
+                for (i in 0 until palabraSinOcultar.size) {
+                    if (letraMetida == palabraSinOcultar[i]) {
+                        palabraOcultada[i] = letraMetida
+                    }
+                }
+                consola.mostrarInfo("Correcto!")
+                consola.mostrarInfo("Adivina la palabra ${palabraOcultada.joinToString(" ")}")
+            } else {
+                numIntentos--
+                consola.mostrarInfo("Incorrecto!, numero de vidas restantes = $numIntentos")
+                consola.mostrarInfo("Adivina la palabra ${palabraOcultada.joinToString(" ")}")
+            }
+
+            if ("_" !in palabraOcultada){
+                palabraTerminada = true
+                consola.mostrarInfo("Felicidades, adivinaste la palabra '$palabraRandom' con $numIntentos numeros de intentos restantes!")
+            }
+            if (numIntentos == 0) {
+                consola.mostrarInfo("Lo siento, Se acabaron tus intentos, la palabra era '$palabraRandom'")
+            }
+        }
     }
     
 
